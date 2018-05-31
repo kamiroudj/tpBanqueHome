149d35d36837c18c5c52f0c19c3989a458c9a23b
NaSo77 <boutra.nadir@gmail.com>
2018-05-31 12:38:05 +0200

    Creation des constructeurs de Personne et Client

--- src/domaine/Client.java
+++ src/domaine/Client.java
@@ -3,11 +3,21 @@ package domaine;
 import java.util.List;
 
 public class Client extends Personne {
-	
-	private String typeClient ;
-	private Conseiller conseiller ; 
-	List <Compte> comptes ; 
-	private CarteBancaire carte; 
+
+	private String typeClient;
+	private Conseiller conseiller;
+	List<Compte> comptes;
+	private CarteBancaire carte;
+
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client(String nom, String prenom, String adresse, String codePostal, String ville, String typeClient) {
+		super(nom, prenom, adresse, codePostal, ville);
+		this.typeClient = typeClient;
+	}
 
 	public String getTypeClient() {
 		return typeClient;
@@ -15,7 +25,6 @@ public class Client extends Personne {
 
 	public void setTypeClient(String typeClient) {
 		this.typeClient = typeClient;
-	} 
-	
+	}
 
 }
